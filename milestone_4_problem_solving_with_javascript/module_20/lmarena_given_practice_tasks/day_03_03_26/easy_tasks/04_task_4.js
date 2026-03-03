ba9e570619c7df1a4e2224cd4d39436d5f5d26fc
ownist <shahedevise@gmail.com>
2026-03-03 23:58:08 +0600

    chore(easy-tasks): Add solution for task 4

---
+++ milestone_4_problem_solving_with_javascript/module_20/lmarena_given_practice_tasks/day_03_03_26/easy_tasks/04_task_4.js
@@ -0,0 +1,9 @@
+/**
+ * Task 4: অ্যারের শেষ এলিমেন্ট (Last Element of Array)
+ *
+ * ৫টি ফলের নামের একটি অ্যারে তৈরি করুন। array.length - 1 লজিকটি ব্যবহার করে ডাইনামিকালি অ্যারের একদম শেষের ফলটি কনসোলে প্রিন্ট করুন।
+ */
+
+const fruits = ["amm", "jamm", "lichu", "kola", "kathal"];
+
+console.log(fruits[fruits.length - 1]);
