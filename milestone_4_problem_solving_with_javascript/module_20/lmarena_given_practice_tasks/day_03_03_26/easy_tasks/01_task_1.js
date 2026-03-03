dd70d20539a8f1467609da8e3aa9c4b1fac841e3
ownist <shahedevise@gmail.com>
2026-03-03 23:45:59 +0600

    feat(easy-tasks): add string concatenation task

---
+++ milestone_4_problem_solving_with_javascript/module_20/lmarena_given_practice_tasks/day_03_03_26/easy_tasks/01_task_1.js
@@ -0,0 +1,10 @@
+/**
+ * Task 1: স্ট্রিং কনক্যাটেনেশন (String Concatenation)
+ *
+ * দুটি ভেরিয়েবল নিন: firstName এবং lastName। এরপর এই দুটি ভেরিয়েবলকে জোড়া লাগিয়ে একটি পূর্ণাঙ্গ নাম (Full Name) তৈরি করুন এবং কনসোলে দেখান। মাঝে যেন স্পেস থাকে।
+ */
+
+let firstName = "Hr";
+let lastName = "Comrade";
+
+console.log(`${firstName} ${lastName}`);
