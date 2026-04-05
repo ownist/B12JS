7ffc6133aea16dd0d2a4f88e3b1795fdace2875d
ownist <shahedevise@gmail.com>
2026-04-05 22:19:41 +0600

    feat(js-tips): add value to boolean conversion tip

---
+++ PROBLEM_SOLVING/lws(js_tips_and_tricks)_js_job_interview_questions/01/02.js
@@ -0,0 +1,12 @@
+// convert any value to boolean
+
+/**
+ * kono value ke boolean e convert korar jonno sei value er age duita !! sign bosiye dilei seita boolean value hoye jai
+ */
+console.log(!!"shahed"); // true
+console.log(!!1); // true
+console.log(!!0); // false
+console.log(!!undefined); // false
+
+// Boolean function use korew eita shortcut way te kora jay
+console.log(Boolean("shahed"));
