d9c907bb14c8deec0dbca23d806a84c8b76d1bad
ownist <shahedevise@gmail.com>
2026-04-15 23:53:06 +0600

    recap: 21-7 Simple calculator to call function inside a function

---
+++ milestone_4/problem_solving_with_javascript/module_21/js_simple_coding_problems_part_2/calculator.js
@@ -0,0 +1,33 @@
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+function calculator(a, b, operation) {
+  if (operation === "add") {
+    const result = add(a, b);
+    return result;
+  } else if (operation === "subtract") {
+    const result = subtract(a, b);
+  } else if (operation === "multiply") {
+    return multiply(a, b);
+  } else if (operation === "divide") {
+    return divide(a, b);
+  } else {
+    return `Only "add", "subtract", "multiply" and "divide" operation is allowed`;
+  }
+}
+
+const result = calculator(5, 7, "multiply");
+console.log(result);
