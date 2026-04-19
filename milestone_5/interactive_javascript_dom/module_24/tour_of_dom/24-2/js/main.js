5cdad8f77ffed55e75332eb43c807b84fde875a8
ownist <shahedevise@gmail.com>
2026-04-19 23:17:39 +0600

    recap: 24-2 Traversing dom - getElementsByTagName

---
+++ milestone_5/interactive_javascript_dom/module_24/tour_of_dom/24-2/js/main.js
@@ -0,0 +1,19 @@
+const liCollections = document.getElementsByTagName("li");
+console.log(liCollections);
+
+// loop html collections
+for (const li of liCollections) {
+  console.log(li.innerText);
+}
+
+console.log("\n"); // new line
+
+// all h1 tags
+const allHeadings = document.getElementsByTagName("h1");
+for (const h1 of allHeadings) {
+  console.log(h1);
+
+  console.log("\n");
+
+  console.log(h1.innerText);
+}
