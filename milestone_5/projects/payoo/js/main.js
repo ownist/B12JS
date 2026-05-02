e51148486a0925c191a83fc31c7615cf2f00bc4d
ownist <shahedevise@gmail.com>
2026-05-02 20:46:08 +0600

    feat(auth): add basic user authentication, 26-4 Login Functionality & Redirect to Home
    - Implement login validation in `main.js`
    - Redirect to dashboard on valid credentials
    - Create `dashboard.html` for post-login

--- milestone_5/projects/payoo/js/main.js
+++ milestone_5/projects/payoo/js/main.js
@@ -3,5 +3,22 @@ const btnContinue = document.getElementById("btnContinue");
 // login button functionality
 btnContinue.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log(e);
+
+  // valid phone number and pin number
+  const phoneNumber = "01234567891";
+  const pinNumber = "1234";
+
+  // get users data
+  const userPhoneNumber = document.getElementById("phone").value;
+  const userPinNumber = document.getElementById("pin").value;
+
+  // check user data and redirect to the dashboard page
+  if (
+    parseInt(userPhoneNumber) === parseInt(phoneNumber) &&
+    parseInt(pinNumber) === parseInt(pinNumber)
+  ) {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid phone number and pin. Tray again!");
+  }
 });
